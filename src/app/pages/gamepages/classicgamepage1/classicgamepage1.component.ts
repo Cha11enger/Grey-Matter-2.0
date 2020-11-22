2e10dd017c19e5f4d7fb00b601aad1acd1921e66
beneta kennedy <benetabenetabeneta@gmail.com>
2020-11-23 02:07:36 +0530

    commit

--- src/app/pages/gamepages/classicgamepage1/classicgamepage1.component.ts
+++ src/app/pages/gamepages/classicgamepage1/classicgamepage1.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, OnDestroy, Input} from "@angular/core";
 import { HttpClient } from '@angular/common/http';
 import Chart from "chart.js";
+import * as $ from "jquery";
+import * as jQuery from 'jquery';
 
 @Component({
   selector: "app-classicgamepage1",
@@ -9,6 +11,11 @@ import Chart from "chart.js";
 })
 export class ClassicgamepageComponent1 implements OnInit, OnDestroy {
   isCollapsed = true;
+  wid1;
+  hei1;
+ 
+ 
+
   constructor(private http: HttpClient) {
   
   }
@@ -16,14 +23,31 @@ export class ClassicgamepageComponent1 implements OnInit, OnDestroy {
   ngOnInit() {
     var body = document.getElementsByTagName("body")[0];
     body.classList.add("landing-page");
+
+   // $(document).ready(function(e) {
+   //   $('img[usemap]').rwdImageMaps();
+  //});
+  
+    
+  //  jQuery.noConflict();
+  //  $(document).ready(function(e) {
+   //   $('img[usemap]').rwdImageMaps();
+
+//  });
+ // this.wid1 = document.getElementById('playerbar').offsetWidth ;
+ // this.hei1 = document.getElementById('playerbar').offsetHeight;
   
   }
+  
+
   ngOnDestroy() {
     var body = document.getElementsByTagName("body")[0];
     body.classList.remove("landing-page");
   }
 
- 
+  yellow(){
+    alert('yellowbox');
+  }
 
   
 
