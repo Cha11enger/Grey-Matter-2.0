36228adc6db769c896906711f077d73142af599e
beneta kennedy <benetabenetabeneta@gmail.com>
2020-11-29 00:30:50 +0530

    commit

--- src/app/pages/gamepages/classicgamepage3/classicgamepage3.component.ts
+++ src/app/pages/gamepages/classicgamepage3/classicgamepage3.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, OnDestroy, Input} from "@angular/core";
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpDownloadProgressEvent } from '@angular/common/http';
 import Chart from "chart.js";
+import { Howler, Howl } from 'howler';
 
 @Component({
   selector: "app-classicgamepage3",
@@ -9,6 +10,13 @@ import Chart from "chart.js";
 })
 export class ClassicgamepageComponent3 implements OnInit, OnDestroy {
   isCollapsed = true;
+  redkey = false;
+bluekey = false;
+  yellowkey = false;
+  greenkey = false;
+
+  audio = new Audio();
+
   constructor(private http: HttpClient) {
   
   }
@@ -24,10 +32,34 @@ export class ClassicgamepageComponent3 implements OnInit, OnDestroy {
   }
 
   expandPiece(piece){
+    if(piece == 'r'){  this.redkey = true;  }
+    if(piece == 'y'){  this.yellowkey = true;  }
+    if(piece == 'b'){  this.bluekey = true;  }
+    if(piece == 'g'){  this.greenkey = true;  }
+
+    
 console.log(piece);
+
+var sound = new Howl({
+  src: ['sound.mp3']
+});
+
+sound.play();
+
+
+
+
+
   }
 
 
+  hidePiece(piece){
+    if(piece == 'r'){  this.redkey = false;  }
+    if(piece == 'y'){  this.yellowkey = false;  }
+    if(piece == 'b'){  this.bluekey = false;  }
+    if(piece == 'g'){  this.greenkey = false;  }
+
+  }
   
 
 
